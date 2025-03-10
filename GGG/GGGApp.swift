a9603a7835483253c10595b8336cb3d8422da931
irismake <iris3455@gmail.com>
2025-03-10 16:29:30 +0900

    Initial Commit

---
+++ GGG/GGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  GGGApp.swift
+//  GGG
+//
+//  Created by 김가희 on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
