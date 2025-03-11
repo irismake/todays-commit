e2a0e723655959886997dc3bfaa969341c062c25
irismake <iris3455@gmail.com>
2025-03-11 23:33:00 +0900

    chore : Apply code convention

--- GGG/GGGApp.swift
+++ GGG/GGGApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct GGGApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
